31b6ac454f1b5354fc0f57976a3d13f34dc3077a
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-24 13:08:00 -0700

    added stories route to app js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/todos', require('./routes/todos'));
+app.use('/stories', require('./routes/stories'));
 app.use('/', require('./routes/index'));
 
 // catch 404 and forward to error handler
