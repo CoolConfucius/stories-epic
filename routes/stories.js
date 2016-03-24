fe75c5f746ab68bf397edd99dbc7061e8acb9f6d
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-24 11:49:53 -0700

    changed names in stories route

--- routes/stories.js
+++ routes/stories.js
@@ -2,32 +2,32 @@
 
 var express = require('express');
 var router = express.Router();
-var Todo = require('../models/todo');
+var Story = require('../models/story');
 
 router.get('/', function(req, res, next) {
-  console.log("getting todos");
-  Todo.find({}, function(err, todos){
+  console.log("getting stories");
+  Story.find({}, function(err, stories){
     if(err) return res.status(400).send(err); 
-    // console.log("Found them,", todos);
-    console.log("Found todos!");
-    res.send(todos); 
+    // console.log("Found them,", stories);
+    console.log("Found stories!");
+    res.send(stories); 
   });
 });
 
 router.post('/', function(req, res, next) {
-  console.log("post todo", req.body);
-  Todo.add(req.body, function(err, todo){
-    res.send(err || todo);
+  console.log("post story", req.body);
+  Story.add(req.body, function(err, story){
+    res.send(err || story);
   });
 });
 
 router.put('/:id', function(req, res, next) {
-  console.log("post todo", req.params.id);
-  Todo.findById(req.params.id, function(err, todo){
+  console.log("post story", req.params.id);
+  Story.findById(req.params.id, function(err, story){
     if(err) return res.status(400).send(err); 
-    console.log("Found one,", todo);
-    todo.iscomplete = !todo.iscomplete; 
-    todo.save(function(err, savedTodo){
+    console.log("Found one,", story);
+    story.iscomplete = !story.iscomplete; 
+    story.save(function(err, savedTodo){
 
       res.send(err || savedTodo);
     })
@@ -35,12 +35,12 @@ router.put('/:id', function(req, res, next) {
 });
 
 router.delete('/:id', function(req, res, next) {
-  console.log("post todo", req.params.id);
-  Todo.findById(req.params.id, function(err, todo){
+  console.log("post story", req.params.id);
+  Story.findById(req.params.id, function(err, story){
     if(err) return res.status(400).send(err); 
-    console.log("Found one,", todo);
-    todo.remove(function(err){
-      res.status(err ? 400 : 200).send(err || todo);
+    console.log("Found one,", story);
+    story.remove(function(err){
+      res.status(err ? 400 : 200).send(err || story);
     })
   });
 });
