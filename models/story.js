4fc0b43f0aac17d8e4f50e62be207dea55425f7b
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-24 11:47:10 -0700

    story model start date

--- models/story.js
+++ models/story.js
@@ -6,7 +6,7 @@ var Story;
 
 var storySchema = mongoose.Schema({
   title: { type: String}, 
-  date: { type: Date, default: Date.now() },
+  startdate: { type: Date, default: Date.now() },
   long: { type: String, default: moment().format('MM/DD/YYYY, h:mm a') }, 
   short: { type: String, default: moment().format('MM/DD/YYYY') }, 
   isprivate: { type: Boolean } 
