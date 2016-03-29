8c89b18c44be4d913c39a9293bde9642e745a37f
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-29 16:43:39 -0700

    story state angular

--- public/js/module.js
+++ public/js/module.js
@@ -9,6 +9,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
   .state('register', { url: '/register', templateUrl: 'html/register.html', controller: 'mainCtrl' })
   .state('createstory', { url: '/createstory', templateUrl: 'html/createstory.html', controller: 'mainCtrl' })
   .state('profile', { url: '/profile', templateUrl: 'html/profile.html', controller: 'mainCtrl' })
+  .state('story', { url: '/story', templateUrl: 'html/story.html', controller: 'mainCtrl' })
   $urlRouterProvider.otherwise('/');
 });
 
