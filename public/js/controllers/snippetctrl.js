97bed7112baefb5fa6a6263081b0c2c42cdf0d77
Benjamin Huang <coolconfucius@yahoo.com>
2016-04-01 00:07:26 -0700

    scope save changes

--- public/js/controllers/snippetctrl.js
+++ public/js/controllers/snippetctrl.js
@@ -3,22 +3,19 @@ app.controller('snippetCtrl', function($scope, $rootScope, $state, $stateParams,
 
   console.log("in snippetCtrl");  
   var snippetid = $state.params.snippetid;
+  
+  $scope.editcontent = '';
 
   Snippet.read(snippetid)
   .then(function(res) {
     console.log("read snippet");
     $scope.snippet = res.data; 
+    $scope.editcontent = res.data.content; 
   });
 
-
-  $scope.isediting = false; 
-  $scope.editsnippet = function(snippet, user) {
-    console.log("edit snippet!", snippet, user);
-    if (!user) return;
-    if (snippet.writtenby !== user.config.data.username) return;
-    console.log("Edit snippet! Passed error handling");
-    $scope.isediting = !$scope.isediting; 
-  }
+  $scope.savechanges = function(editcontent){
+    console.log(editcontent);
+  };
 
   $scope.isdeleting = false; 
   $scope.deletesnippet = function(snippet, user) {
