a520a464b656fef086454e5b8365742e5e53283e
Benjamin Huang <coolconfucius@yahoo.com>
2016-04-01 16:50:42 -0700

    commented out console logs in snippetctrl

--- public/js/controllers/snippetctrl.js
+++ public/js/controllers/snippetctrl.js
@@ -1,20 +1,20 @@
 app.controller('snippetCtrl', function($scope, $rootScope, $state, $stateParams, $localStorage, Story, Snippet ) {
   $rootScope.user = $localStorage.token; 
 
-  console.log("in snippetCtrl");  
+  // console.log("in snippetCtrl");  
   var snippetid = $state.params.snippetid;
   
   $scope.editcontent = '';
 
   Snippet.read(snippetid)
   .then(function(res) {
-    console.log("read snippet");
+    // console.log("read snippet");
     $scope.snippet = res.data; 
     $scope.editcontent = res.data.content; 
   });
 
   $scope.savechanges = function(editcontent){
-    console.log(editcontent);
+    // console.log(editcontent);
     Snippet.edit(snippetid, editcontent).then(function(){
       $scope.snippet.content = editcontent;
     });
@@ -22,10 +22,10 @@ app.controller('snippetCtrl', function($scope, $rootScope, $state, $stateParams,
 
   $scope.isdeleting = false; 
   $scope.deletesnippet = function(snippet, user) {
-    console.log("deletesnippet snippet!", snippet, user);
+    // console.log("deletesnippet snippet!", snippet, user);
     if (!user) return;
     if (snippet.writtenby !== user.config.data.username) return;
-    console.log("Delete snippet! Passed error handling");
+    // console.log("Delete snippet! Passed error handling");
     $scope.isdeleting = !$scope.isdeleting; 
   }
 
