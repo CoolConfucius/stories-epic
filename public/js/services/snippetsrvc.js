a5e360baa0276e40193c78a9ae67b85e5dcd376e
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-31 23:33:14 -0700

    added snippet read in snippet service

--- public/js/services/snippetsrvc.js
+++ public/js/services/snippetsrvc.js
@@ -6,4 +6,9 @@ app.service('Snippet', function($http) {
   this.add = function(snippet) {
     return $http.post(`/snippets`, snippet)
   };
+
+  this.read = function(snippetid){
+    return $http.get(`/snippets/${snippetid}`)
+  }
+
 });
