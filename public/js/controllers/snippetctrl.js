0aeced81c7ba3b2a51f917239fe2036545e31649
Benjamin Huang <coolconfucius@yahoo.com>
2016-03-31 23:31:43 -0700

    made snippet ctrl

---
+++ public/js/controllers/snippetctrl.js
@@ -0,0 +1,20 @@
+app.controller('snippetCtrl', function($scope, $rootScope, $state, $stateParams, $localStorage, Story, Snippet ) {
+  $rootScope.user = $localStorage.token; 
+  
+  var snippetid = $state.params.snippetid;
+
+  Snippet.read(snippetid)
+  .then(function(res) {
+    $scope.story = res.data; 
+  });
+
+
+  $scope.isediting = false; 
+  $scope.editsnippet = function(snippet, user) {
+    console.log("edit snippet!", snippet, user);
+    if (!user) return;
+    if (snippet.writtenby !== user.config.data.username) return;
+    console.log("Edit snippet! Passed error handling");
+    $scope.isediting = !$scope.isediting; 
+  }
+});
