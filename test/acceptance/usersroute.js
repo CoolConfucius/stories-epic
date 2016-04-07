028fda34819c3e11c25276b48f67668dfd197429
Benjamin Huang <coolconfucius@yahoo.com>
2016-04-06 22:30:47 -0700

    made mocha tests

---
+++ test/acceptance/usersroute.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var supertest = require('supertest'); 
+var mongoose = require('mongoose'); 
+var expect = require('chai').expect;
+
+var app = require('../../app');
+var User = require('../../models/user'); 
+
+
+const dbUrl = 'mongodb://localhost/introtoauth-test';
+
+before(function(done) {
+  mongoose.connection.close(function() {
+    mongoose.connect(dbUrl, function(err) {
+      done(err);
+    });
+  });
+});
+
+
+after(function(done) {
+  mongoose.connection.close(function() {
+    done();
+  });
+});
+
+beforeEach(function(done) {
+  User.remove({}, function(err){
+    if (err) return done(err);
+    var testUser = { username: 'user1', password: 'very secure' };
+    User.register(testUser, function(err, user) {
+      done(err); 
+    }); 
+  });
+})
+
+describe('users route', function() {
+  describe('GET /users/:username', function() {
+
+    it('should return user of username.', function(done) {
+
+      supertest(app)
+        .get('/users/:user1')
+        .end(function(err, res) {
+
+          expect(res.statusCode).to.equal(200); 
+          done(); 
+        })
+    });
+
+  });
+
+
+  describe('POST /users/login', function() {
+
+    var agent = supertest.agent(app);
+
+    it('should login a user, and set a cookie with a token.', function(done) {
+      agent
+        .post('/users/login')
+        .send({'username':'user1', password: 'very secure'})
+        .expect(200)
+        // .expect('set-cookie', /cookie\=.+/)
+        .end(done);
+
+    });
+
+  });
+});
+
+
---
+++ test/unit/usermodel.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var path = require('path'); 
+var chai = require('chai'); 
+var mongoose = require('mongoose'); 
+var expect = chai.expect; 
+
+var User = require(path.join(__dirname, '../../models/user')); 
+
+const dbUrl = 'mongodb://localhost/introtoauth-test';
+
+before(function(done) {
+  mongoose.connection.close(function() {
+    mongoose.connect(dbUrl, function(err) {
+      done(err);
+    });
+  });
+});
+
+
+after(function(done) {
+  mongoose.connection.close(function() {
+    done();
+  });
+});
+
+beforeEach(function(done) {
+  User.remove({}, function(err){
+    if (err) return done(err);
+    var testUser = { username: 'user1', password: 'very secure' };
+    User.register(testUser, function(err, user) {
+      done(err); 
+    }); 
+  });
+})
+
+describe('User', function() {
+  
+  describe('.register()', function() {
+
+    it('should register a new user, with a valid username and password.', function(done) {
+    
+      var testUser = {username: 'mrdude', password: 'very secure'};
+    
+      User.register(testUser, function(err, user){
+        expect(err).to.be.null;
+        expect(user.username).to.equal(testUser.username); 
+        expect(user.password).to.be.null; 
+        done(); 
+      })
+    });
+
+    it('should NOT register a user - username already taken.', function(done) {
+    
+      var testUser = {username: 'user1', password: 'very secure'};
+    
+      User.register(testUser, function(err, user){
+        expect(err).to.exist; 
+        expect(user).to.not.exist; 
+        done(); 
+      })
+      
+    });
+  });
+});
+
+
